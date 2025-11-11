44bb502a7cefa50fc314c84941129364cba176cc
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-10 21:45:14 -0400

    make appimage script

---
+++ make-appimage.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+VERSION="$(cat ~/version)"
+
+export OUTPUT_APPIMAGE=1
+export ADD_HOOKS="self-updater.bg.hook"
+export ICON=/usr/share/icons/hicolor/128x128/apps/audacity.png
+export DESKTOP=/usr/share/applications/org.audacityteam.Audacity.desktop
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export OUTNAME=Audacity-"$VERSION"-anylinux-"$ARCH".AppImage
+
+# Deploy dependencies
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun /usr/bin/audacity
